8d279230fd4425aa5d01f44fc2ad1abdde4168fb
Sadanydv <95264696+Sadanydv@users.noreply.github.com>
2024-09-20 15:13:16 +0530

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,36 +1,48 @@
-import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { useState } from "react";
 import data from "../assets/data.json";
+import image1 from "../assets/left-arrow.png";
+import image2 from "../assets/right-arrow.png";
 
 const Work = () => {
+  const [currentProject, setCurrentProject] = useState(0);
+  const totalProjects = data.projects.length;
+
+  const handleNext = () => {
+    setCurrentProject((prev) => (prev + 1) % totalProjects);
+  };
+
+  const handlePrev = () => {
+    setCurrentProject((prev) => (prev - 1 + totalProjects) % totalProjects);
+  };
+
   return (
     <div id="work">
-      <h2>WORK</h2>
+      <h2 className="headingwork">Projects</h2>
       <section>
         <article>
-          <Carousel
-            showArrows={false}
-            showIndicators={false}
-            showStatus={false}
-            showThumbs={false}
-            interval={2000}
-            infiniteLoop={true}
-            autoPlay={true}
-          >
-            {data.projects.map((i) => (
-              <div key={i.title} className="workItem">
-                <img src={i.imgSrc} alt={i.title} />
-                <aside>
-                  <h3>{i.title}</h3>
-                  <p>{i.description}</p>
-                  <a target={"blank"} href={i.url}>
-                    View Demo
-                  </a>
-                </aside>
-              </div>
-            ))}
-          </Carousel>
+          <div key={data.projects[currentProject].title} className="workItem">
+            <img
+              src={data.projects[currentProject].imgSrc}
+              alt={data.projects[currentProject].title}
+            />
+            <aside>
+              <h3 className="projectheading">
+                {data.projects[currentProject].title}<span> (Project {data.projects[currentProject].number})</span>
+              </h3>
+              <p>{data.projects[currentProject].description}</p>
+              <a target={"_blank"} href={data.projects[currentProject].url}>
+                View Demo
+              </a>
+            </aside>
+          </div>
+          <div className="navigation-buttons">
+            <button className="button1" onClick={handlePrev}>
+              <img src={image1} alt="Previous" />
+            </button>
+            <button className="button2" onClick={handleNext}>
+              <img src={image2} alt="Next" />
+            </button>
+          </div>
         </article>
       </section>
     </div>
